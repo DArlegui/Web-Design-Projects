dbb3d4a8d34ffb92fe0601ce76b593bd4c21309f
DArlegui <arleguidaniel@gmail.com>
2023-08-28 03:06:01 -0700

    Added removeItem & clearItems functions

--- Shopping-List/script.js
+++ Shopping-List/script.js
@@ -1,15 +1,16 @@
-const itemForm = document.getElementById("item-form");
-const itemInput = document.getElementById("item-input");
+const itemForm = document.getElementById("item-form"); //Form
+const itemInput = document.getElementById("item-input"); //Input inside the
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
 function addItem(e) {
-  e.preventDefault(); //We don't want the form to actually submit to the file
+  e.preventDefault();
 
   const newItem = itemInput.value;
 
-  //Validate Input
-  if (itemInput.value === "") {
-    alert("Please add an item");
+  //Validation
+  if (newItem === "") {
+    alert("Please add something");
     return;
   }
 
@@ -17,6 +18,7 @@ function addItem(e) {
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
 
+  //Creating a new button and appending it
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
 
@@ -28,16 +30,29 @@ function addItem(e) {
 function createButton(classes) {
   const button = document.createElement("button");
   button.className = classes;
-  const icon = createIcon("fa-solid fa-xmark");
-  button.appendChild(icon);
+
+  //Creating an icon inside this function instead
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-xmark";
+
+  button.appendChild(icon); //Appending the icon inside button
   return button;
 }
 
-function createIcon(classes) {
-  const icon = document.createElement("i");
-  icon.className = classes;
-  return icon;
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
 }
 
-//Event Listeners
+function clearItems() {
+  console.log("works");
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
+
+// Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
